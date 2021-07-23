10665319b35d5ec2cc61442ef6c43af4a8bff7e0
jpromanonet <programming@jpromano.net>
2021-07-23 00:25:29 -0300

    Product card

--- ecommerce-rjs/src/App.js
+++ ecommerce-rjs/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Product from './components/Product'
 
 function App() {
   return (
     <div className="App">
       <h1>Test!</h1>
+      <Product />
     </div>
   );
 }
